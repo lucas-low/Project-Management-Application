923fd9df1b45bac912d736ab56508f7639af485b
Lucas Low <lucas-low@outlook.com>
2022-06-14 12:22:52 +0800

    added pages

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Using MERN tech stack aka MongoDB, Express, React, Nodejs, GraphQL and a Apollo with Apollo/GraphQL Client to build an intuitive project management application that scales. 
 
-Design pre-build with Figma then 
-I referred back to University of Helsinki at some key problems at:-
+Design build with Figma then 
+I referred back to University of Helsinki at some key problems then figured out the rest at:-
 [Node.js, Express, mongoose models, schema, MongoDB](https://fullstackopen.com/en/part3/saving_data_to_mongo_db)
 [React, create-react-app](https://fullstackopen.com/en/part1/component_state_event_handlers)
 [React, GraphQL-server, Mutations](https://fullstackopen.com/en/part8/react_and_graph_ql)
--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Header from "./components/Header";
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 import Clients from "./components/Clients";
+import Projects from "./components/Projects";
 import AddClientModal from "./components/AddClientModal";
 
 const cache = new InMemoryCache({
@@ -31,11 +33,14 @@ function App() {
   return (
     <>
       <ApolloProvider client={client}>
+      <Router>
         <Header />
           <div className='container'>
           <AddClientModal />
+          <Projects />
            <Clients />
           </div> 
+      </Router>
     </ApolloProvider>
     </>
   );
